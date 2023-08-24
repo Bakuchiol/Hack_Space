ea1e1d14b3e4d634fb6376181a6b27041cece6d9
Lynette Cargo <lynettecargo@gmail.com>
2023-08-24 18:21:22 -0500

    50 images 👽

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import key from './API/key'
+// import Nasa from './components/Nasa';
 
 function App() {
 
 
- const [nasa, setNasa] = useState("")
+ const [imgData, setImgData] = useState([0])
 
  const apikey = key
 
  const getImage = async () =>{
-  const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apikey}`);
+  const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apikey}&count=50`);
   const data = await response.json();
   console.log(data);
-    
-    setNasa(data)
+    setImgData(data)
  }
 
  useEffect(() =>{
   getImage()
  }, [])
 
-
-
-
   return (
     <div className="App">
       {/* <h1>Hello World</h1> */}
-        <img src={nasa.url} alt=""/>
-        <h1>{nasa.title}</h1>
-        {nasa.explanation}
+      {/* <Nasa data={data}/> */}
+
+
+      {imgData.map((img,i) => {
+        return (
+          <div key={i}>
+            <img src={img.url} alt=''/>
+          </div>
+        )
+      })}
+
+        {/* <img src={imgData.url} alt=""/>
+        <h1>{imgData.title}</h1>
+        <p>{imgData.explanation}</p> */}
     </div>
   );
 }
--- src/components/Nasa.jsx
+++ src/components/Nasa.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-function Nasa() {
+function Nasa(props) {
+
+    // const nasa = props.data
+
   return (
     <div>
-      
+      <h1>hi</h1>
+      {props.data.map((img,i) => {
+        return (
+            <div key={i}>
+                <img src={img.url} alt="" />
+            </div>
+        )
+      })}
     </div>
   )
 }
